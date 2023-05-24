386343d9cea342b3dd9e1b680cc7b474e8e4fec9
Philippe Skopal <philippe.skopal@scorechain.com>
2023-05-24 16:03:23 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Tea tea = new Tea();
+        tea.prepareRecipe();
+
+        Coffee coffee = new Coffee();
+        coffee.prepareRecipe();
+    }
+}
---
+++ src/Tea.java
@@ -0,0 +1,14 @@
+public class Tea extends Beverage {
+    public void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    public void addCondiments() {
+        System.out.println("Adding Lemon");
+    }
+
+    @Override
+    public void boilWater() {
+        System.out.println("Boiling water at 70");
+    }
+}
